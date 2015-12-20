a4117d13c4eb7189a7ce96bb16e6a538057c4637
loku92 <danielpawlow@gmail.com>
2015-12-20 22:51:45 +0100

    Logic - dodano modele Point i Route

---
+++ MyTrip.MyTripLogic/Models/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTrip.MyTripLogic.Models
+{
+    public class Point
+    {
+        public int id { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string city { get; set; }
+    }
+}
---
+++ MyTrip.MyTripLogic/Models/Route.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTrip.MyTripLogic.Models
+{
+    public class Route
+    {
+
+        public Route()
+        {
+            this.points = new List<Point>();
+        }
+
+        public int id { get; set; }
+        public List<Point> points { get; set; }
+    }
+}
