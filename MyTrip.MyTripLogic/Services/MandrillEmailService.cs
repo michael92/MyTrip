804ba5862562a46192d94fb3a9a81288a83b8bf3
Marcin Bartoś <bartosmarcin@gmail.com>
2016-01-18 20:13:37 +0100

    Added SendPasswordResetEmail method

--- MyTrip.MyTripLogic/Services/MandrillEmailService.cs
+++ MyTrip.MyTripLogic/Services/MandrillEmailService.cs
@@ -1,10 +1,36 @@
-﻿namespace MyTrip.MyTripLogic.Services
+﻿using Mandrill;
+using Mandrill.Models;
+using Mandrill.Requests.Messages;
+using Microsoft.Azure;
+using MyTrip.MyTripLogic.Models;
+using System.Threading.Tasks;
+
+namespace MyTrip.MyTripLogic.Services
 {
     public class MandrillEmailService
     {
+        private readonly MandrillApi _api;
+
         public MandrillEmailService()
         {
+            var apiKey = CloudConfigurationManager.GetSetting("MandrillApiKey");
+            _api = new MandrillApi(apiKey);
+        }
 
+        public async Task SendPasswordResetEmail(MyTripUser user, string token)
+        {
+            var email = new EmailAddress(user.Email);
+
+            var message = new EmailMessage
+            {
+                Tags = new[] { "passreset" },
+                To = new[] { email }
+            };
+
+            message.AddGlobalVariable("username", user.UserName);
+            message.AddGlobalVariable("token", token);
+
+            var result = await _api.SendMessage(new SendMessageRequest(message));
         }
     }
 }
