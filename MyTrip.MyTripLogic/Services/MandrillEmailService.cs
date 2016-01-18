cd86bf561b633609c094ebb1a10912829fea54bf
Marcin Bartoś <bartosmarcin@gmail.com>
2016-01-18 21:52:25 +0100

    Fixed SendPasswordResetEmail to use templated messages

--- MyTrip.MyTripLogic/Services/MandrillEmailService.cs
+++ MyTrip.MyTripLogic/Services/MandrillEmailService.cs
@@ -4,6 +4,8 @@ using Mandrill.Models;
 using Mandrill.Requests.Messages;
 using Microsoft.Azure;
 using MyTrip.MyTripLogic.Models;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyTrip.MyTripLogic.Services
@@ -11,6 +13,7 @@ namespace MyTrip.MyTripLogic.Services
     public class MandrillEmailService
     {
         private readonly MandrillApi _api;
+        private string passwordResetUrl = "test.pl";
 
         public MandrillEmailService()
         {
@@ -22,16 +25,34 @@ namespace MyTrip.MyTripLogic.Services
         {
             var email = new EmailAddress(user.Email);
 
+            var url = GetUrl(user.Id, token);
+
             var message = new EmailMessage
             {
+                FromEmail = "noreplay@mytrip.com",
                 Tags = new[] { "passreset" },
                 To = new[] { email }
             };
 
             message.AddGlobalVariable("username", user.UserName);
-            message.AddGlobalVariable("token", token);
+            message.AddGlobalVariable("token", url);
 
-            var result = await _api.SendMessage(new SendMessageRequest(message));
+            var request = new SendMessageTemplateRequest(message, "passreset", Enumerable.Empty<TemplateContent>());
+            var result = await _api.SendMessageTemplate(request);
+            var c = result.Count;
+        }
+
+        private string GetUrl(string userId, string token)
+        {
+            var queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
+
+            queryString["userid"] = userId;
+            queryString["token"] = token;
+
+            var ub = new UriBuilder(passwordResetUrl);
+            ub.Query = queryString.ToString();
+
+            return ub.ToString();
         }
     }
 }
