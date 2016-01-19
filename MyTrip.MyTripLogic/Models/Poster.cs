fb955452a6c5d6e63c2e9085bcddc9edb8d7a96e
Dariusz Pawłowski <pawlowski2015@gmail.com>
2016-01-19 10:57:32 +0100

    Plakat

---
+++ MyTrip.MyTripLogic/Models/Poster.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTrip.MyTripLogic.Models
+{
+    public class Poster
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public PosterStatus PosterStatus { get; set; }
+
+        public string TripId { get; set; }
+
+        public string Url { get; set; }
+
+      
+
+    }
+}
